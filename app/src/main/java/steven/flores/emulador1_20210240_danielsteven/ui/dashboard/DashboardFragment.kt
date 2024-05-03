87f2836f3a99482bf35f0b71c9f30493b3ee2f7a
StevenDispox <20210240@ricaldone.edu.sv>
2024-05-02 18:56:49 -0600

    mitad de proyecto

---
+++ app/src/main/java/steven/flores/emulador1_20210240_danielsteven/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,53 @@
+package steven.flores.emulador1_20210240_danielsteven.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import steven.flores.emulador1_20210240_danielsteven.R
+import steven.flores.emulador1_20210240_danielsteven.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        val btnImprimir = root.findViewById<Button>(R.id.btnImprimir)
+
+        btnImprimir.setOnClickListener {
+
+            Toast.makeText(context,"Steven Palacios",Toast.LENGTH_SHORT).show()
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
